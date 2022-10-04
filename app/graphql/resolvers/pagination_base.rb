ad10d55e7dd18fd21627e1e0b62340960eeb3cb2
JigmeT123 <tashinamgyaljigme@gmail.com>
2022-10-04 17:09:21 +0400

    feat(): pagination

--- app/graphql/resolvers/pagination_base.rb
+++ app/graphql/resolvers/pagination_base.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'search_object/plugin/graphql'
-
 module Resolvers
   class PaginationBase < SearchObjectBase
     option(:skip, type: Int) { |scope, value| scope.offset(value) }
