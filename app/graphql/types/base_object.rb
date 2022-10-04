15cc44aad620a37cf2c05c8c69e725f346caca67
JigmeT123 <tashinamgyaljigme@gmail.com>
2022-10-04 17:23:27 +0400

    feat(): documents url

--- app/graphql/types/base_object.rb
+++ app/graphql/types/base_object.rb
@@ -3,6 +3,7 @@
 module Types
   class BaseObject < GraphQL::Schema::Object
     include GraphqlHelper
+    include Rails.application.routes.url_helpers
 
     edge_type_class(Types::BaseEdge)
     connection_type_class(Types::BaseConnection)
--- app/graphql/types/profile_type.rb
+++ app/graphql/types/profile_type.rb
@@ -5,5 +5,12 @@ module Types
     field :id, ID, null: false
     field :cid_or_passport, String, null: false
     field :kyc_status, Types::KycStatusEnum, null: false
+    field :documents_url, [String], null: false
+
+    def documents_url
+      object.documents.blobs.each_with_object([]) do |blob, array|
+        array << rails_blob_url(blob, only_path: true)
+      end
+    end
   end
 end
