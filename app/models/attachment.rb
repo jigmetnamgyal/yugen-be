b54a3f9316eb540f25a9c9ad1dff6100839cf730
JigmeT123 <tashinamgyaljigme@gmail.com>
2022-10-04 08:51:02 +0400

    feat(): profile and attachments relationshi

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,4 +1,26 @@
 # frozen_string_literal: true
 
 class Attachment < ApplicationRecord
+  include Rails.application.routes.url_helpers
+
+  belongs_to :user
+  belongs_to :attachable, polymorphic: true, optional: true
+
+  has_one_attached :file
+
+  validates :category, presence: true
+  validates :file_type, presence: true
+  validates :file_size, presence: true, numericality: true
+  validates :file,
+            attached: true,
+            file_size: { less_than: 40.megabytes },
+            file_content_type: {
+              allow: %w[text/csv image/jpeg image/jpg image/png application/vnd.ms-excel]
+            }
+
+  enum category: { avatar: 0, cid_or_passport: 1 }
+
+  def url
+    rails_blob_url(file.blob)
+  end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class Profile < ApplicationRecord
+  has_one :profile, dependent: :destroy
+  has_many :attachments, as: :attachable, dependent: :destroy
 end
