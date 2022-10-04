a9b8e40ab023568898a829171f1253b2fb818f92
JigmeT123 <tashinamgyaljigme@gmail.com>
2022-10-04 15:49:55 +0400

    added kyc field in the model

--- db/migrate/20221004044124_create_profiles.rb
+++ db/migrate/20221004044124_create_profiles.rb
@@ -5,6 +5,7 @@ class CreateProfiles < ActiveRecord::Migration[7.0]
     create_table :profiles do |t|
       t.references :user, foreign_key: true
       t.string :cid_or_passport, null: false
+      t.integer :kyc_status, null: false, default: 0
 
       t.timestamps
     end
