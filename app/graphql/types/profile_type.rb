0ea6a123b299ca03f64699904a9ff6abf3a3e245
JigmeT123 <tashinamgyaljigme@gmail.com>
2022-10-06 11:31:02 +0400

    feat(): null value check

--- app/graphql/types/profile_type.rb
+++ app/graphql/types/profile_type.rb
@@ -6,7 +6,7 @@ module Types
     field :cid_or_passport, String, null: false
     field :kyc_status, Types::KycStatusEnum, null: false
     field :documents_url, [String], null: false
-    field :avatar_url, String, null: false
+    field :avatar_url, String, null: true
 
     def documents_url
       object.documents.blobs.each_with_object([]) do |blob, array|
@@ -15,6 +15,8 @@ module Types
     end
 
     def avatar_url
+      return if object.avatar.blob.nil?
+
       rails_blob_url(object.avatar.blob, only_path: true)
     end
   end
