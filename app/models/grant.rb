ce8cb45942735982e7f9f3421661aa0e2d64b80c
JigmeT123 <tashinamgyaljigme@gmail.com>
2022-10-11 08:46:41 +0400

    fixed(): type

--- app/models/grant.rb
+++ app/models/grant.rb
@@ -3,5 +3,5 @@
 class Grant < ApplicationRecord
   validates :description, :title, presence: true
 
-  enum grant_review_status: { inactive: 0, approved: 1, rejected: 2 }g
+  enum grant_review_status: { inactive: 0, approved: 1, rejected: 2 }
 end
