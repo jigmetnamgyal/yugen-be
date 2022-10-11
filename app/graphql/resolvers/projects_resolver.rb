0a6e4ff8009bf0326ddb40e3f0a7b0e2237ea9a9
JigmeT123 <tashinamgyaljigme@gmail.com>
2022-10-11 12:07:55 +0400

    feat(): projects resolver

--- app/graphql/resolvers/projects_resolver.rb
+++ app/graphql/resolvers/projects_resolver.rb
@@ -8,12 +8,18 @@ module Resolvers
 
     type Types::GrantType.connection_type, null: false
 
+    option :ideas, type: Boolean, with: :filter_projects
+
     option :query, type: String, with: :apply_search, description: <<~DESC
       Supports searches on project title
     DESC
 
     def apply_search(scope, value)
       scope.where("CONCAT_WS(' ', project_title) iLIKE ?", "%#{value.squish}%")
+    end
+
+    def filter_projects(scope, value)
+      value ? scope.where(grant_id: nil) : scope.where.not(grant_id: nil)
     end
   end
 end
