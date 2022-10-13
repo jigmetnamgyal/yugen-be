2983880094895be46856ec756aa26e9b3073c2da
JigmeT123 <tashinamgyaljigme@gmail.com>
2022-10-13 04:42:26 +0400

    feat(): payment type filter

--- app/graphql/resolvers/orders_resolver.rb
+++ app/graphql/resolvers/orders_resolver.rb
@@ -7,5 +7,11 @@ module Resolvers
     scope { Order.where(payment_completed: false) }
 
     type Types::OrderType.connection_type, null: false
+
+    option :payment_type, type: Types::PaymentTypeEnum, with: :filter_orders
+
+    def filter_orders(scope, value)
+      scope.where(payment_type: value)
+    end
   end
 end
