34b7b4947a4d5fdd14ef9d218999dea3ca3943ef
JigmeT123 <tashinamgyaljigme@gmail.com>
2022-10-14 16:12:43 +0400

    feat(): fixed cors issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://yugen-web.vercel.app/'
+    origins 'http://localhost:3000', 'https://yugen-web.vercel.app'
 
     resource '*',
              headers: :any,
